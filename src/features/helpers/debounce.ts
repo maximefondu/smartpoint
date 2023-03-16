a706a988510e98bd1b050cedcd3a7d541b8ada4e
Maxime Fondu <maximefondu@icloud.com>
2023-03-16 10:55:14 +0100

    Add function helpers debounce

---
+++ src/features/helpers/debounce.ts
@@ -0,0 +1,14 @@
+import { useCallback } from 'react'
+
+export const debounce = (fn: Function, ms = 300) => {
+    let timeoutId: ReturnType<typeof setTimeout>
+
+    return function (this: any, ...args: any[]) {
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => fn.apply(this, args), ms)
+    }
+}
+
+export const useDebounce = (fn: Function, ms = 300) => {
+    return useCallback(debounce(fn, ms), [])
+}
